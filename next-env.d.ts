3fc33846e67753060329344ba93ead557a0729b4
ndstrzz <andynilessim05@gmail.com>
2026-03-02 21:10:48 +0800

    testcs

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,22 +1,6 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
+import "./.next/dev/types/routes.d.ts";
 
-import type React from "react";
-
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      "model-viewer": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
-        src?: string;
-        "camera-controls"?: boolean;
-        "auto-rotate"?: boolean;
-        "rotation-per-second"?: string;
-        "disable-zoom"?: boolean;
-        "shadow-intensity"?: string;
-        exposure?: string;
-      };
-    }
-  }
-}
-
-export {};
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
