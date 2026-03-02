4aaca394fa41ee954fe4024782641b5be9f0ddff
ndstrzz <andynilessim05@gmail.com>
2026-03-02 12:50:17 +0800

    Add model viweew tyoing

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,10 +1,20 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
+import type React from "react";
+
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      "model-viewer": any;
+      "model-viewer": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+        src?: string;
+        "camera-controls"?: boolean;
+        "auto-rotate"?: boolean;
+        "rotation-per-second"?: string;
+        "disable-zoom"?: boolean;
+        "shadow-intensity"?: string;
+        exposure?: string;
+      };
     }
   }
 }
